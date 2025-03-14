d911815ff927e0f210c8a996e8cf1261cb958997
AMelnikk <amelnikk81@gmail.com>
2025-03-14 11:45:08 +0100

    ...

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\olive\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\olive\StudioProjects\Tennis-Vereinsapp"
+export "FLUTTER_ROOT=/Users/danielaugustin/Downloads/Tennis-Vereinsapp/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/danielaugustin/Documents/Tennis-Vereinsapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
